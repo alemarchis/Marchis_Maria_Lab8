c410b3bd07de838cf7b6a702cf24cbda391233c4
Alexandra Marchis <marchisalexandra15@gmail.com>
2021-11-24 16:02:29 +0200

    Add project files.

---
+++ Data/Marchis_Maria_Lab8Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Marchis_Maria_Lab8.Models;
+
+namespace Marchis_Maria_Lab8.Data
+{
+    public class Marchis_Maria_Lab8Context : DbContext
+    {
+        public Marchis_Maria_Lab8Context (DbContextOptions<Marchis_Maria_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Marchis_Maria_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Marchis_Maria_Lab8.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Marchis_Maria_Lab8.Data;
+using Marchis_Maria_Lab8.Models;
+
+namespace Marchis_Maria_Lab8.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Marchis_Maria_Lab8.Data.Marchis_Maria_Lab8Context _context;
+
+        public IndexModel(Marchis_Maria_Lab8.Data.Marchis_Maria_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Publisher = await _context.Publisher.ToListAsync();
+        }
+    }
+}
